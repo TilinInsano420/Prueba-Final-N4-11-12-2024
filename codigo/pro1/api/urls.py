611794830aabcfa33ed81f5fb0bb5aeb12577cd0
Dylan Carmona <169170035+TilinInsano420@users.noreply.github.com>
2024-12-16 18:59:05 -0300

    CAMBIOS DE VIDA
    viewsets god.

--- codigo/pro1/api/urls.py
+++ codigo/pro1/api/urls.py
@@ -1,11 +1,9 @@
-from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet, MascotaViewSet, ConsultaViewSet
 
-urlpatterns = [
-    path('users/', views.user_list, name='user_list'),
-    path('users/<int:pk>/', views.user_detail, name='user_detail'),
-    path('mascotas/', views.mascota_list, name='mascota_list'),
-    path('mascotas/<int:pk>/', views.mascota_detail, name='mascota_detail'),
-    path('consultas/', views.consulta_list, name='consulta_list'),
-    path('consultas/<int:pk>/', views.consulta_detail, name='consulta_detail'),
-]
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'mascotas', MascotaViewSet)
+router.register(r'consultas', ConsultaViewSet)
+
+urlpatterns = router.urls
